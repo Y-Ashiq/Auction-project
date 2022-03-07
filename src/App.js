e69869208945238f0ebe77c9c6e0a8f63c2a4d61
Ashiq <y.alashiq11@gmail.com>
2022-03-07 04:40:23 +0200

    adding the project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
+import { Redirect, Route, Switch } from "react-router-dom";
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Bid from './components/Bid/Bid';
+
+class App extends Component {
+    render() {
+        return (
+             <>
+                < Navbar/>
+                < Switch >
+
+                    < Route path="/home" component={Home}/> 
+                    < Route path="/register" component={Register} /> 
+                    < Route path="/login" component={Login} /> 
+                    < Route path="/Bid" component={Bid} />
+                     <Redirect exact from="/" to="/home" />
+                    < Route path="*" component={NotFound} />
+     </Switch>
+                    </>
+           
+      );
+    }
+}
+
+ export default App;
---
+++ src/components/Bid/Bid.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bid extends Component {
+    render() {
+        return (
+            <div>
+               Bid
+            </div>
+        );
+    }
+}
+
+export default Bid;
